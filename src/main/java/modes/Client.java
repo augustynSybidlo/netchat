b8fb93aad6aa4f0c6a5e05e86767a6589aee06c6
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-07-31 15:26:26 +0200

    Modify Client class to implement runnable interface

--- src/main/java/modes/Client.java
+++ src/main/java/modes/Client.java
@@ -3,13 +3,16 @@ package modes;
 import java.net.*;
 import java.io.*;
 
-public class Client {
+public class Client implements Runnable {
     private Socket socket = null;
+    private Thread thread = null;
     private DataInputStream console = null;
     private DataOutputStream streamOut = null;
+    private ClientThread client = null;
 
     public Client(String serverName, int serverPort) {
         System.out.println("Establishing connection. Please wait ...");
+
         try {
             socket = new Socket(serverName, serverPort);
             System.out.println("Connected: " + socket);
@@ -19,29 +22,52 @@ public class Client {
         } catch(IOException ioe) {
             System.out.println("Unexpected exception: " + ioe.getMessage());
         }
-        String line = "";
-        while (!line.equals(".bye")) {
+    }
+
+    public void run() {
+        while(thread != null) {
             try {
-                line = console.readLine();
-                streamOut.writeUTF(line);
+                streamOut.writeUTF(console.readLine());
                 streamOut.flush();
             } catch(IOException ioe) {
                 System.out.println("Sending error: " + ioe.getMessage());
+                stop();
             }
         }
     }
 
-    private void start() throws IOException {
-        console = new DataInputStream(System.in);
-        streamOut = new DataOutputStream(socket.getOutputStream());
+    public void handle(String msg) {
+        if(msg.equals(".bye")) {
+            System.out.println("Good bye. Press RETURN to exit ...");
+            stop();
+        } else {
+            System.out.println(msg);
+        }
     }
-    public void stop() {
+    private void start() throws IOException {
+        console   = new DataInputStream(System.in);
+        streamOut = new DataOutputStream(socket.getOutputStream());
+        if (thread == null) {
+            client = new ClientThread(this, socket);
+            thread = new Thread(this);
+            thread.start();
+        }
+    }
+
+    private void stop() {
+        if (thread != null) {
+            thread.stop();
+            thread = null;
+        }
+
         try {
             if (console != null) console.close();
             if (streamOut != null) streamOut.close();
             if (socket != null) socket.close();
-        } catch(IOException ioe) {
+        } catch (IOException ioe) {
             System.out.println("Error closing ...");
         }
+        client.close();
+        client.stop();
     }
 }
