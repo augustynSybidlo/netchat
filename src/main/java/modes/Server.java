51a58a8337404498cabb50431f75016b8f1612c5
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-07-31 14:38:23 +0200

    Add constructor to Server class

--- src/main/java/modes/Server.java
+++ src/main/java/modes/Server.java
@@ -1,4 +1,20 @@
 package modes;
 
-public class Server {
+import java.net.*;
+import java.io.*;
+
+public class Server implements Runnable
+{  private ServerSocket     server = null;
+    private Thread           thread = null;
+    private ServerThread client = null;
+
+    public Server(int port) {
+        try {
+            System.out.println("Binding to port " + port + ", please wait  ...");
+            server = new ServerSocket(port);
+            System.out.println("Server started: " + server);
+            start();
+        } catch(IOException ioe) {
+            System.out.println(ioe); }
+    }
 }
