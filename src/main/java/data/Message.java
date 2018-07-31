6b5cc353cb0e0173cc424af818c099122c71d017
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-07-31 13:53:38 +0200

    Add getters/setters to Message class

--- src/main/java/data/Message.java
+++ src/main/java/data/Message.java
@@ -13,4 +13,29 @@ public class Message implements Serializable {
         this.author = author;
         this.createdAt = LocalDateTime.now();
     }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "content='" + content + '\'' +
+                ", author='" + author + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
 }
